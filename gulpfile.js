82873d1584e385c01b7fa8cc7e4ee769ecfdc6da
SFTutukov <SFTutukov@yandex.ru>
2016-11-11 12:30:37 +0300

    init
    Signed-off-by: SFTutukov <SFTutukov@yandex.ru>

--- gulpfile.js
+++ gulpfile.js
@@ -20,46 +20,86 @@ $ npm install gulp-sass --save-dev
 compass (gulp-compass)
 $ npm install gulp-compass --save-dev
 
+Sprite (gulp-sprite)
+npm install --save-dev gulp-sprite
+
 LiverReload (gulp-connect)
 $ npm install --save-dev gulp-connect
 
 Jade (gulp-jade)
-npm install --save-dev gulp-jade
+$ npm install --save-dev gulp-jade
+
+Merge (merge-stream)
+$ npm install --save-dev merge-stream
+
+Concat (gulp-concat)
+$ npm install --save-dev gulp-concat
+
+Minify images (gulp-image-optimization)
+$ npm install --save-dev gulp-image-optimization
 */
 
 "use strict";
 
 var gulp 			= require('gulp'),
-	// concatCSS 		= require('gulp-concat-css'),
+	concatCSS 		= require('gulp-concat-css'),
 	sass			= require('gulp-sass'),
+    spritesmith     = require('gulp.spritesmith'),
 	rename			= require('gulp-rename'),
  	minifyCSS 		= require('gulp-minify-css'),
  	autoprefixer 	= require('gulp-autoprefixer'),
  	notify 			= require('gulp-notify'),
-    jade            = require('gulp-jade');
- 	// connect 		= require('gulp-connect');
+    jade            = require('gulp-jade'),
+    concat          = require('gulp-concat'),
+    stream          = require('merge-stream'),
+    image           = require('gulp-image'),
+ 	connect 		= require('gulp-connect');
  
 
 //GLOBAL TAST
-	gulp.task('global',['connect','html','less','watch']);
+	gulp.task('global',['watch','connect']);
 
 //watch
 gulp.task('watch', function(){
 	gulp.watch('./src/sass/**/*.scss',['sass']);
 	gulp.watch('./src/jade/**/*.jade',['jade']);
-	// gulp.watch('index.html',['html']);
+    gulp.watch('./src/js/**/*.js',['js']);
+    gulp.watch('./src/images/sprite/*.png',['sprite', 'sass']);
 });
-
 
 //sass
 gulp.task('sass', function () {
-    return gulp.src('./src/sass/**/*.scss')
-        .pipe(sass())
+    var sassStream,
+        cssStream;
+
+    sassStream = gulp.src('./src/sass/common.scss')
+        .pipe(sass());
+
+    cssStream = gulp.src('./src/css/**/*.css');
+    return stream(cssStream, sassStream)
+        .pipe(concatCSS('common.css'))
         .pipe(autoprefixer({browsers: ['last 2 versions'], cascade: false}))
         .pipe(minifyCSS())
-        .pipe(rename("common.css"))
         .pipe(gulp.dest('./dist/css'))
-        .pipe(notify('DONE!'));
+        .pipe(connect.reload());
+});
+
+//sprite
+gulp.task('sprite', function () {
+    var spriteData = gulp.src("./src/images/sprite/*.png")
+        .pipe(spritesmith({
+            imgName:'sprite.png',
+            cssName:'_sprite.scss',
+            imgPath: '../images/sprite/sprite.png'
+        }));
+    
+    var imgStream = spriteData.img
+        .pipe(gulp.dest('./dist/images/sprite'));
+
+    var cssStream = spriteData.css
+        .pipe(gulp.dest('./src/sass/base'));
+
+    return stream(imgStream, cssStream);
 });
 
 //jade
@@ -69,45 +109,48 @@ gulp.task('jade', function() {
             pretty: true
         }))
         .pipe(gulp.dest('./dist/'))
+        .pipe(connect.reload());
 });
 
-// gulp.task('less', function () {
-//   return gulp.src('./src/sass/*.scss')
-//     .pipe(less())
-//     .pipe(csso())
-//     .pipe(autoprefixer({browsers: ['last 2 versions'], cascade: false}))
-//     .pipe(minifyCSS())
-//     .pipe(rename("template_styles.css"))
-//     .pipe(gulp.dest('app/css'))
-//     .pipe(connect.reload())
-//     .pipe(notify('DONE!'));
-// });
 
-//html
-gulp.task('html', function () {
-	return gulp.src("index.html")
-	.pipe(gulp.dest('app'))
-	.pipe(connect.reload())
-	.pipe(notify('DONE!'));
+//js
+gulp.task('js', function() {
+    var libs = gulp.src('./src/js/libs/*.js')
+        .pipe(concat('libs.js'))
+        .pipe(gulp.dest('./dist/js/libs'));
+
+    var scripts = gulp.src('./src/js/*.js')
+        .pipe(gulp.dest('./dist/js'));
+
+    return stream(libs, scripts)
+        .pipe(connect.reload());
 });
 
 //server connect
 gulp.task('connect', function() {
   connect.server({
-    root: 'app',
+    root: 'dist',
+    port: 8000,
     livereload: true
   });
 });
 
 //Minify images
-// gulp.task('images', function(cb) {
-//     gulp.src(['images/**/*.png','images/**/*.jpg','images/**/*.gif','images/**/*.jpeg']).pipe(imageop({
-//         optimizationLevel: 5,
-//         progressive: true,
-//         interlaced: true
-//     })).pipe(gulp.dest('app/images-min/')).on('end', cb).on('error', cb)
-//     .pipe(notify('DONE!'));
-// });
+gulp.task('images', function() {
+    gulp.src(['!./src/images/sprite/**/*.png','./src/images/**/*.png','./src/images/**/*.jpg','./src/images/**/*.gif','./src/images/**/*.jpeg'])
+        .pipe(image({
+          pngquant: true,
+          optipng: false,
+          zopflipng: true,
+          jpegRecompress: false,
+          jpegoptim: true,
+          mozjpeg: true,
+          gifsicle: true,
+          svgo: true,
+          concurrent: 10
+        })).pipe(gulp.dest('./dist/images/'))
+        .pipe(notify('DONE!'));
+});
 
 
 //Remove unused CSS selectors
